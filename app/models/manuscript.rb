3666e73fa182d4ddee11bc24bb0050f8948e1c03
James Naadjie <james.naadjie@gmail.com>
2015-07-09 06:27:46 -0400

    minor fixes

--- app/models/manuscript.rb
+++ app/models/manuscript.rb
@@ -12,23 +12,16 @@ class Manuscript
   	find_by(code: code)
   end
 
-
-  def get_authors
-  	authors
-  end
-
   def author_match?(author_params)
   	lastname = author_params.strip.downcase
-  	manuscript_authors = self.get_authors
+  	manuscript_authors = self.authors
   	manuscript_authors.select {|author| author_match_to_params(author.last_name.downcase,lastname)}.any?
   end
 
   private
   
   def author_match_to_params(author_lastname, author_params)
-  	author_lastname.start_with?(author_params[0,3]) && 1 < author_params.length &&
-    (author_params.length == author_lastname.length || 2 < author_params.length )
+  	author_lastname.start_with?(author_params[0,3]) && author_params.length > 1 &&
+    (author_params.length == author_lastname.length || author_params.length > 2 )
   end
-
-
 end
--- spec/models/manuscript_spec.rb
+++ spec/models/manuscript_spec.rb
@@ -1,41 +1,41 @@
 require 'spec_helper'
 
 describe Manuscript do
-	let(:valid_attributes) do
-      Hash[code: 'AA1001', title: 'some title', status: 'NEW', :status_date => Date.today]
+  let(:valid_attributes) do
+    Hash[code: 'AA1001', title: 'some title', status: 'NEW', :status_date => Date.today]
+  end
+
+  describe 'persistence' do
+    it 'creates with valid attributes' do
+      Manuscript.create!(valid_attributes)
     end
+  end
 
-	describe 'persistence' do
-	  it 'creates with valid attributes' do
-	    Manuscript.create!(valid_attributes)
-	  end
-	end
-
-    describe 'manuscript status query' do
-  	  before :each do
-  	   	@manuscript = Manuscript.create!(valid_attributes)
-  		@manuscript.authors << Author.new(publish_name: 'Li,John')
-  		@exitent_author_params = "li"
-  		@nonexitent_author_params = "Witherspoon"
-  	  end
-      describe '#get authors' do
-        it "should get all manuscript authors" do
-  	      expect(@manuscript.get_authors).to eq(@manuscript.authors)
-        end
-      end
-
-      describe "#author_match?" do
-        it "should be true with valid author" do
-  	      expect(@manuscript.author_match?(@exitent_author_params)).to eq(true)
-        end
-
-        it "should ignore white space" do
-          expect(@manuscript.author_match?(" Li")).to eq(true)
-        end
-
-         it "should be false with invalid author" do
-  	      expect(@manuscript.author_match?(@nonexitent_author_params)).to eq(false)
-        end
+  describe 'manuscript status query' do
+    before :each do
+      @manuscript = Manuscript.create!(valid_attributes)
+      @manuscript.authors << Author.new(publish_name: 'Li,John')
+      @exitent_author_params = 'li'
+      @nonexitent_author_params = 'Witherspoon'
+    end
+    describe '#get authors' do
+      it 'should get all manuscript authors' do
+        expect(@manuscript.get_authors).to eq(@manuscript.authors)
       end
     end
+
+    describe '#author_match?' do
+      it 'should be true with valid author' do
+        expect(@manuscript.author_match?(@exitent_author_params)).to eq(true)
+      end
+
+      it 'should ignore white space' do
+        expect(@manuscript.author_match?(' Li')).to eq(true)
+      end
+
+      it 'should be false with invalid author' do
+        expect(@manuscript.author_match?(@nonexitent_author_params)).to eq(false)
+      end
+    end
+  end
 end
